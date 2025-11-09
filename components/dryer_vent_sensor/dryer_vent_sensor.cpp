f51ff00307996fe14788337e401262de3fa31af1
Keith Baker <syzygy@specialks.com>
2025-11-09 18:18:33 -0500

    or not

--- components/dryer_vent_sensor/dryer_vent_sensor.cpp
+++ components/dryer_vent_sensor/dryer_vent_sensor.cpp
@@ -11,7 +11,7 @@ static uint16_t current_item = 0;
 static hw_timer_t *timer = nullptr;
 
 void IRAM_ATTR DryerVentSensor::timer_isr(void *arg) {
-  int32_t count;
+  int count;
   pcnt_unit_handle_t unit = (pcnt_unit_handle_t)arg;
   pcnt_unit_get_count(unit, &count);
   ring[current_item] = count;
